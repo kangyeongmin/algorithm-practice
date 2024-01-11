77f3034b6c3a3fc98aedd82a9bcedd187ce7509f
강영민 <kym6050@hanyang.ac.kr>
2024-01-11 12:36:01 +0900

    [level 3] Title: 섬 연결하기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -0,0 +1,27 @@
+function solution(n, costs) {
+    var answer = 0;
+    let parent = Array.from({length: n}, (_, i) => i)
+    let edge = 0
+    costs = costs.sort((a,b) => a[2] - b[2])
+    
+    for(const costArr of costs){
+        const [a,b,cost] = costArr
+        if(parent[a] === parent[b]) continue
+        
+        if(parent.filter((p) => p === parent[b]).length >
+           parent.filter((p) => p === parent[a]).length)
+            {
+            parent = parent.map((p) => p === parent[a] ? p = parent[b] : p)
+            }
+        else {
+            parent = parent.map((p) => p === parent[b] ? p = parent[a] : p)
+        }
+        answer += cost
+        edge++
+        
+        if(edge === n-1) break
+    }
+    
+    
+    return answer;
+}
