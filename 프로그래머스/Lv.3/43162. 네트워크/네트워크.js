2a059a61b4b6c2e4fed9e6c75560edac4db2f717
강영민 <kym6050@hanyang.ac.kr>
2024-01-15 15:32:56 +0900

    [level Lv.3] Title: 네트워크, Time: 11.38 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/43162. 네트워크/네트워크.js
@@ -0,0 +1,22 @@
+function solution(n, computers) {
+    let answer = 0;
+    const visited = [];
+    
+    for(let i = 0; i < n; i++) {
+        if(!visited[i]) {
+            dfs(i, visited, computers);	
+            answer++;	
+        }
+    }
+    
+    return answer;
+}
+
+function dfs(node, visited, computers) {
+    visited[node] = true;	
+    console.log(visited)
+    for(let i = 0; i < computers.length; i++) {
+        if(computers[node][i] === 1 && !visited[i]) 	
+            dfs(i, visited, computers);	
+    }
+}
