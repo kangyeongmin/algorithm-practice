8aa563f305ae58e6ece29e5bfa0cd357d5219caf
강영민 <kym6050@hanyang.ac.kr>
2024-01-18 09:49:21 +0900

    [level 3] Title: 베스트앨범, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,35 @@
+function solution(genres, plays) {
+    var answer = [];
+    const playlist = {}
+    const order = []
+    
+    for(let i=0; i<genres.length; i++){
+        if(!playlist[genres[i]]){
+            playlist[genres[i]] = []
+        }
+        
+        playlist[genres[i]].push([plays[i],i])
+    }
+    Object.keys(playlist).map((genre) => {
+        playlist[genre] = playlist[genre].sort((a,b) => {
+            if(b[0] === a[0]) return a[1] - b[1] 
+            return b[0]-a[0]
+        })
+    })
+
+    Object.keys(playlist).map((genre, index) => {
+        const sum = playlist[genre].reduce((accumulator, currentValue) => accumulator                     + currentValue[0], 0,);
+        order.push([genre, sum])
+    })
+    
+    order.sort((a,b) => b[1] - a[1])
+    order.forEach((best) => {
+        answer.push((playlist[best[0]][0][1]))
+        if(playlist[best[0]][1])
+            answer.push((playlist[best[0]][1][1]))
+    })
+    
+    // console.log(playlist, order)
+    
+    return answer;
+}
